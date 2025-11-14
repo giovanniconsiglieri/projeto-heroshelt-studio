8426232fcd812fa885c97986a96021c69ea9e439
giovanniconsiglieri <giovanniconsiglieri.dev@gmail.com>
2025-11-14 20:16:15 -0300

    adicionado background e hover no header

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Olá JavaScript!")
+
+const cabeçalho = document.getElementsByClassName("link-header")
+console.log(cabeçalho)
+
+const img = document.getElementsByTagName("img")
+console.log(img)
